97d4058dc185aa4082007a758649a554a13b1fd9
Yan Yue on Ubunt <yanyue1991@gmail.com>
2014-11-17 02:33:25 -0800

    d

--- scripts/stats_avg_flow_time.sh
+++ scripts/stats_avg_flow_time.sh
@@ -33,5 +33,5 @@ do
     #echo ${serverHost}
     #echo ${serverPort}
     python send.py ${serverHost} ${serverPort} "nPackets $num_packets ${avg_time}"
-    python send.py localhost 50006 "nPackets $num_packets ${avg_time}"
+    python send.py localhost 50006 "localnPackets ${serverHost} $num_packets ${avg_time}"
 done
