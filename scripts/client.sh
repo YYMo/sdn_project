60f4264dd183541bb3c22d257cfe43cacff0e4b7
ubuntu <yanyue1991@gmail.com>
2014-11-17 19:01:13 -0800

    df

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf command.txt
 sudo python server.py &
 ./commander.sh &
 ./monitor.sh 192.168.44.129 &
--- scripts/server.sh
+++ scripts/server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm -rf command.txt
 sudo python server.py &
 ./commander.sh &
